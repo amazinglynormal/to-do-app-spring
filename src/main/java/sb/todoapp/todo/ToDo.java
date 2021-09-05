29c9fbb51df2316892cc45fd338e00c342422d4c
Stephen <stephenbrady365@gmail.com>
2021-09-05 16:02:24 +0100

    Add basic ToDo entity

---
+++ src/main/java/sb/todoapp/todo/ToDo.java
@@ -0,0 +1,51 @@
+package sb.todoapp.todo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "ToDo")
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    public ToDo() {
+    }
+
+    public ToDo(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
