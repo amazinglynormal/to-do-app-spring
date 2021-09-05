7cd9d351f8415b0874dfb028951821f5085034a3
Stephen <stephenbrady365@gmail.com>
2021-09-05 16:14:01 +0100

    Add UserRepository interface

---
+++ src/main/java/sb/todoapp/user/UserRepository.java
@@ -0,0 +1,7 @@
+package sb.todoapp.user;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByEmail(String email);
+}
