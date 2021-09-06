ee3aaec46714ae62e0ee562131257f2b976c4b2b
Stephen <stephenbrady365@gmail.com>
2021-09-06 20:30:35 +0100

    Add UserController

---
+++ src/main/java/sb/todoapp/user/UserController.java
@@ -0,0 +1,24 @@
+package sb.todoapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    User createNewUser(@RequestBody User newUser) {
+        return userService.createNewUser(newUser);
+    }
+}
