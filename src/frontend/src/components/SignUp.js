375ad487c2cfd98789b462a1bf88e9aafa27b8e5
Stephen <stephenbrady365@gmail.com>
2021-09-13 20:26:27 +0100

    Add form to sign up page

---
+++ src/frontend/src/components/SignUp.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Form, Input, Button } from "antd";
+import { Container } from "./Container";
+
+import "./FormContainer.css";
+
+const SignUp = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Container>
+      <h1>Sign Up Page</h1>
+      <div className="formContainer">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Name"
+            name="name"
+            rules={[{ required: true, message: "Please input your name" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email" }]}
+          >
+            <Input type="email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Create account
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Container>
+  );
+};
+
+export default SignUp;
