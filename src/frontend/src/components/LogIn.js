17d40de4f8b7961a575380d7cecde4540ac9cb83
Stephen <stephenbrady365@gmail.com>
2021-09-13 20:26:14 +0100

    Add form to log in page

---
+++ src/frontend/src/components/LogIn.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Form, Input, Button } from "antd";
+import { Container } from "./Container";
+
+import "./FormContainer.css";
+
+const LogIn = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <Container>
+      <h1>Log In Page</h1>
+      <div className="formContainer">
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email" }]}
+          >
+            <Input type="email" />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </Container>
+  );
+};
+
+export default LogIn;
