908286f5961ff6929e15d3216514eeaced290b94
Stephen <stephenbrady365@gmail.com>
2021-09-15 19:49:38 +0100

    Add new routes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,31 +1,43 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Navbar } from "./components/Navbar";
-import "./App.css";
 
+import { UserContext } from "./state/user-context";
+import { Navbar } from "./components/Navbar";
+import { AddNewToDoList } from "./components/AddNewToDoList";
+import { ToDoList } from "./components/ToDoList";
 import Home from "./components/Home";
 import LogIn from "./components/LogIn";
 import SignUp from "./components/SignUp";
 import Profile from "./components/Profile";
 
+import "./App.css";
+
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
-        <Switch>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/login">
-            <LogIn />
-          </Route>
-          <Route path="/signup">
-            <SignUp />
-          </Route>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
+        <UserContext.Provider value={{}}>
+          <Navbar />
+          <Switch>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/login">
+              <LogIn />
+            </Route>
+            <Route path="/signup">
+              <SignUp />
+            </Route>
+            <Route path="/todolist/:id">
+              <ToDoList />
+            </Route>
+            <Route exact path="/todolist">
+              <AddNewToDoList />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </UserContext.Provider>
       </Router>
     </>
   );
